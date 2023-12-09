9b0e73abcc511f42a1744238270c69db122571f6
jordan7510 <cgupta7510@gmail.com>
2023-12-09 13:08:50 +0530

    port changed back to 8080

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ require("./app/routes/userreviews.routes.js")(app);
 require("./app/routes/messagetoteacher.routes.js")(app);
 
 // set port, listen for requests
-// const PORT = process.env.PORT || 8080;
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
+// const PORT = process.env.PORT || 80;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
