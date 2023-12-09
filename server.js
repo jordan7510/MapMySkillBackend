b5ba6e101ccc9f7050e5c47738d75acd4229c788
jordan7510 <cgupta7510@gmail.com>
2023-12-09 14:43:35 +0530

    port changed to 3000

--- server.js
+++ server.js
@@ -50,9 +50,11 @@ require("./app/routes/messagetoteacher.routes.js")(app);
 
 // set port, listen for requests
 // const PORT = process.env.PORT || 8080;
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
+
+
 
 app.use("/uploads",express.static('uploads'))
