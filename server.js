5bf006227756387571eaef874c851ffd9ab320d7
jordan7510 <cgupta7510@gmail.com>
2023-12-11 14:05:23 +0530

    cors enabled

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
-// const cors = require("cors");
+const cors = require("cors");
+app.use(cors);
 
 // var corsOptions = {
 //   origin: "http://localhost:5173",
 // };
 
 // app.use(cors(corsOptions));
-// app.use(cors);
 
 app.use(express.json());
 
